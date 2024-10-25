bca31e3cebe9a24805e1550714c447184bdf7562
AA22_Gi <gibert22@mail.ru>
2024-10-26 02:10:32 +0700

    Написана логика функции create_user

--- app/routers/user.py
+++ app/routers/user.py
@@ -21,18 +21,32 @@ async def all_users(db: Annotated[Session, Depends(get_db)]):
 @router.get('/user_id')
 async def user_by_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
     user_ = db.scalars(select(User).where(User.id == user_id))
-    if user is None:
+    if user_ is None:
         raise HTTPException(status_code=404, detail="User  was not found")
     return user_
 
 
 @router.post('/create')
-async def create_user(create_user: CreateUser, db: Annotated[Session, Depends(get_db)]):
-    pass
+async def create_user(create_new_user: CreateUser, db: Annotated[Session, Depends(get_db)]):
+    # Проверка на существование пользователя
+    result = db.execute(select(User).where(User.username == create_new_user.username)).fetchone()
+    if result is not None:
+        raise HTTPException(status_code=400, detail="User  with this username already exists")
+
+    # Создание нового пользователя
+    db.execute(insert(User).values(
+        username=create_new_user.username,
+        firstname=create_new_user.firstname,
+        lastname=create_new_user.lastname,
+        age=create_new_user.age,
+        slug=slugify(create_new_user.username)
+    ))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
 
 
 @router.put('/update')
-async def update_user():
+async def update_user(user_id: int, update_user: UpdateUser, db: Session = Depends(get_db)):
     pass
 
 
