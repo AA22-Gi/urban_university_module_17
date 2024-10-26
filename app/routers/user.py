aef738d6c961e99b0e91aa0ff1f1920949a0520f
AA22_Gi <gibert22@mail.ru>
2024-10-26 10:26:40 +0700

    Написана логика функции delete_user

--- app/routers/user.py
+++ app/routers/user.py
@@ -65,5 +65,13 @@ async def update_user(user_id: int, updated_user: UpdateUser , db: Annotated[Ses
 
 
 @router.delete('/delete')
-async def delete_user():
-    pass
+async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    # Проверка на существование пользователя
+    result = db.execute(select(User).where(User.id == user_id)).fetchone()
+    if result is None:
+        raise HTTPException(status_code=404, detail="User  not found")
+
+    # Удаление пользователя
+    db.execute(delete(User).where(User.id == user_id))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User  deleted successfully'}
